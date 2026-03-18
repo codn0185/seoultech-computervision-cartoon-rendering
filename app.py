4342a3a98419cdf13f6d0a97de5da0a3c11eb6bb
codn0185 <113341200+codn0185@users.noreply.github.com>
2026-03-18 14:36:07 +0900

    init: add app.py & cartoon_renderer.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from cartoon_renderer import CartoonRenderer
+
+
+def main():
+    app = CartoonRenderer()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ cartoon_renderer.py
@@ -0,0 +1,10 @@
+import numpy as cnp
+import cv2 as cv
+
+
+class CartoonRenderer:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
